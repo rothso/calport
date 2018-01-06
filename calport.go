edabcde1efb5b4e22e95135dcfd091116f6426c7
Rothanak So <rothanak.hello@gmail.com>
2018-01-06 18:08:52 -0500

    Cache first successful fetch to prevent costly refetches

--- calport.go
+++ calport.go
@@ -6,45 +6,72 @@ import (
 	"github.com/chromedp/chromedp"
 	"time"
 	"fmt"
+	"os"
+	"io/ioutil"
+	"path/filepath"
 )
 
 func main() {
 	var username, password string // TODO: read from CLI
-	var err error
+
+	// Download the schedule page if it hasn't been downloaded yet
+	cacheDir := "./.cache"
+	file := filepath.Join(cacheDir, "schedule.html");
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		log.Println("Logging into myWings...")
+		html, err := getRawSchedule(username, password)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("Success! Saving raw schedule to %s\n", file)
+		err = os.Mkdir(cacheDir, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ioutil.WriteFile(file, []byte(html), 0644)
+	} else {
+		log.Println("Cached schedule found! Nothing to download")
+	}
+
+	// TODO: parse results, integrate with Google Calendar
+}
+
+func getRawSchedule(username, password string) (string, error) {
+	var html string
 
 	// Create context
 	ctxt, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	// Create chrome instance
-	c, err := chromedp.New(ctxt, chromedp.WithDebugf(log.Printf))
+	c, err := chromedp.New(ctxt/*, chromedp.WithDebugf(log.Printf))*/)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
 	// Log in and get the schedule for the current term
-	var res string
-	err = c.Run(ctxt, getSchedule(username, password, &res))
+	err = c.Run(ctxt, scrapeSchedule(username, password, &html))
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
 	// Shutdown chrome
 	err = c.Shutdown(ctxt)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
 	// Wait for chrome to finish
 	err = c.Wait()
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
-	// TODO: parse results, integrate with Google Calendar, add caching/lockfile
+	return html, nil
 }
 
-func getSchedule(username, password string, res *string) chromedp.Tasks {
+func scrapeSchedule(username, password string, res *string) chromedp.Tasks {
 	loginUrl := "https://mywings.unf.edu/"
 	schedUrl := "http://mywings2.unf.edu/cp/ip/login?sys=sctssb" +
 		"&url=https://banner.unf.edu/pls/nfpo/bwskfshd.P_CrseSchdDetl"
@@ -52,7 +79,7 @@ func getSchedule(username, password string, res *string) chromedp.Tasks {
 	loginInput := fmt.Sprintf("%s\t%s\n", username, password);
 	return chromedp.Tasks{
 		chromedp.Navigate(loginUrl),
-		chromedp.Sleep(1 * time.Second), // if we log in too fast, it fails
+		chromedp.Sleep(2 * time.Second), // if we log in too fast, it fails
 		chromedp.SendKeys(`#userID`, loginInput, chromedp.ByID),
 		chromedp.WaitVisible(`#alertAdminMessageDiv`, chromedp.ByID),
 		chromedp.Navigate(schedUrl),
@@ -60,6 +87,6 @@ func getSchedule(username, password string, res *string) chromedp.Tasks {
 		chromedp.Click(`input[type="submit"]`), // fixme: sometimes stuck on this page
 		chromedp.WaitNotPresent(`select`, chromedp.ByQuery),
 		chromedp.WaitVisible(`.pagebodydiv`, chromedp.ByQuery),
-		chromedp.InnerHTML(`.pagebodydiv`, res, chromedp.ByQuery),
+		chromedp.InnerHTML(`.pagebodydiv`, res, chromedp.ByQuery), // TODO: store term name as well
 	}
 }
